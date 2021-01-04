4d71d8d3c02f1779947bfccbdaa3ec6e5f1b2acd
Wojciech Iracki <irackiw@gmail.com>
2021-01-04 22:08:38 +0100

    Move project to mvn; refactor code; add dependencies and test usage of them

---
+++ zad3/src/main/java/com/example/app/InvoiceTaxCalculator.java
@@ -0,0 +1,57 @@
+package com.example.app;
+
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
+
+public class InvoiceTaxCalculator {
+
+    private static final double ipBoxTaxPercentage = 0.05;
+    private static final int splitPaymentRequiredLimitPln = 15000;
+
+
+    public double getVatFromGross(double grossValue, double vatRate) {
+        this.validatePositiveValue(grossValue);
+        return Math.round(grossValue * vatRate / (1.00 + vatRate));
+    }
+
+    public double getVatFromNet(double grossValue, double vatRate) {
+        this.validatePositiveValue(grossValue);
+        return Math.round(grossValue * vatRate);
+    }
+
+    public double getIncomeTaxFromGross(double grossValue, double vatRate, double incomeTaxRate) {
+        this.validatePositiveValue(grossValue);
+        return Math.round((grossValue - this.getVatFromGross(grossValue, vatRate)) * incomeTaxRate);
+    }
+
+    public double getIncomeTaxFromNet(double netValue, double incomeTaxRate) {
+        this.validatePositiveValue(netValue);
+        return netValue * incomeTaxRate;
+    }
+
+    public double getIncomeTaxFromCurrencyUnit(Money money) {
+        this.validatePositiveValue(money.getAmount().doubleValue());
+        return this.getIncomeTaxFromNet(money.getAmount().doubleValue(), ipBoxTaxPercentage);
+    }
+
+    public boolean isSplitPaymentRequired(double value) {
+        this.validatePositiveValue(value);
+        return value > splitPaymentRequiredLimitPln;
+    }
+
+    public boolean isSplitPaymentRequiredForMoney(Money money, String currency) {
+        if (!"PLN".equals(currency)) {
+            throw new IllegalArgumentException("Currency not supported.");
+        }
+        double value = money.getAmount().doubleValue();
+
+        return this.isSplitPaymentRequired(value);
+    }
+
+    protected void validatePositiveValue(double value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("Value must be positive.");
+        }
+    }
+
+}
---
+++ zad3/src/test/java/com/example/app/InvoiceTaxCalculatorTest.java
@@ -0,0 +1,142 @@
+package com.example.app;
+
+import org.joda.money.Money;
+import org.junit.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InvoiceTaxCalculatorTest {
+
+    private InvoiceTaxCalculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new InvoiceTaxCalculator();
+    }
+
+    @After
+    public void tearDown() {
+        calculator = null;
+    }
+
+    @Test
+    public void testCalculatorClassType() {
+        assertTrue(calculator instanceof InvoiceTaxCalculator);
+    }
+
+    @Test
+    public void testGetVatFromGross() {
+        double result = calculator.getVatFromGross(1230.00, 0.23);
+        assertEquals(230.00, result);
+    }
+
+    @Test
+    public void testGetVatFromGrossException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            calculator.getVatFromGross(-5, 0.23);
+        });
+    }
+
+    @Test
+    public void testGetVatFromNet() {
+        double result = calculator.getVatFromNet(1000.00, 0.23);
+        assertEquals(230.00, result);
+    }
+
+    @Test
+    public void testGetVatFromNetException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            calculator.getVatFromNet(-5, 0.23);
+        });
+    }
+
+    @Test
+    public void testGetIncomeTaxFromGross() {
+        double result = calculator.getIncomeTaxFromGross(1230.00, 0.23, 0.19);
+        assertEquals(190.00, result);
+    }
+
+    @Test
+    public void testGetIncomeTaxFromGrossException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            calculator.getIncomeTaxFromGross(-5, 0.23, 0.19);
+        });
+    }
+
+    @Test
+    public void testGetIncomeTaxFromNet() {
+        double result = calculator.getIncomeTaxFromNet(1000.00, 0.19);
+        assertEquals(190.00, result);
+    }
+
+    @Test
+    public void testGetIncomeTaxFromNetException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            calculator.getIncomeTaxFromNet(-1.00, 0.19);
+        });
+    }
+
+    @Test
+    public void testGetIncomeTaxFromCurrencyUnitException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            calculator.getIncomeTaxFromCurrencyUnit(Money.parse("PLN -5.00"));
+        });
+    }
+
+    @Test
+    public void testGetIncomeTaxFromCurrencyUnit() {
+        double result = calculator.getIncomeTaxFromCurrencyUnit(Money.parse("PLN 100.00"));
+        assertEquals(5.00, result);
+    }
+
+    @Test
+    public void testIsSplitPaymentRequiredException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            calculator.isSplitPaymentRequired(-5000.00);
+        });
+    }
+
+    @Test
+    public void testIsSplitPaymentRequiredPositive() {
+        boolean result = calculator.isSplitPaymentRequired(9000.00);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void testIsSplitPaymentRequiredNegative() {
+        boolean result = calculator.isSplitPaymentRequired(9000.00);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void testIsSplitPaymentRequiredForMoneyCurrencyException() {
+        Money money = Money.parse("PLN 100.00");
+        assertThrows(IllegalArgumentException.class, () -> {
+            calculator.isSplitPaymentRequiredForMoney(money, "GBP");
+        });
+    }
+
+    @Test
+    public void testIsSplitPaymentRequiredForMoneyNegativeException() {
+        Money money = Money.parse("PLN -100.00");
+        assertThrows(IllegalArgumentException.class, () -> {
+            calculator.isSplitPaymentRequiredForMoney(money, "PLN");
+        });
+    }
+
+    @Test
+    public void testIsSplitPaymentRequiredForMoneyNegative() {
+        Money money = Money.parse("PLN 100.00");
+        boolean result = calculator.isSplitPaymentRequiredForMoney(money, "PLN");
+
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void testIsSplitPaymentRequiredForMoneyPositive() {
+        Money money = Money.parse("PLN 10005.00");
+        boolean result = calculator.isSplitPaymentRequiredForMoney(money, "PLN");
+
+        assertEquals(false, result);
+    }
+}
