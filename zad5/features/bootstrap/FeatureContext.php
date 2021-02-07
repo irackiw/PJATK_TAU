de9704ae620541b7256ea859ec748eff0ef43ac7
Wojciech Iracki <irackiw@gmail.com>
2021-02-07 15:40:55 +0100

    lab5

---
+++ zad5/features/bootstrap/FeatureContext.php
@@ -0,0 +1,73 @@
+<?php
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context
+{
+    /**
+     * @var Shelf
+     */
+    private $shelf;
+
+    /**
+     * @var Basket
+     */
+    private $basket;
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+        $this->shelf = new Shelf();
+        $this->basket = new Basket($this->shelf);
+    }
+
+    /**
+     * @Given there is a :arg1, which costs £:arg2
+     */
+    public function thereIsAWhichCostsPs($product, $price)
+    {
+        $this->shelf->setProductPrice($product, floatval($price));
+    }
+
+    /**
+     * @When I add the :arg1 to the basket
+     */
+    public function iAddTheToTheBasket($product)
+    {
+        $this->basket->addProduct($product);
+    }
+
+    /**
+     * @Then /I should have ([\d]+) product([s]?) in the basket/
+     */
+    public function iShouldHaveProducstInTheBasket($count)
+    {
+        \PHPUnit\Framework\Assert::assertCount(
+            intval($count),
+            $this->basket
+        );
+    }
+
+    /**
+     * @Then the overall basket price should be £:arg1
+     */
+    public function theOverallBasketPriceShouldBePs($price)
+    {
+        \PHPUnit\Framework\Assert::assertSame(
+            floatval($price),
+            $this->basket->getTotalPrice()
+        );
+    }
+}
